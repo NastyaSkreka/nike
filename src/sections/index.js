0d38a1eb9fd9a625d352470530415eaf096f985a
Nastya <skreka.nastya@gmail.com>
2023-09-10 23:11:00 +0300

    nike

---
+++ src/sections/index.js
@@ -0,0 +1,19 @@
+import CustomerReviews from "./CustomerReviews";
+import Footer from "./Footer";
+import Hero from "./Hero";
+import PopularProducts from "./PopularProducts";
+import Servicer from "./Servicer";
+import SpecialOffer from "./SpecialOffer";
+import Subscribe from "./Subscribe";
+import SuperQuality from "./SuperQuality";
+
+export {
+    CustomerReviews, 
+    Footer, 
+    Hero, 
+    PopularProducts, 
+    Servicer, 
+    SpecialOffer, 
+    Subscribe, 
+    SuperQuality
+}
